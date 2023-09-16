6898ddd290db8f7ea00e2da1dd175df7cdf39b75
Malcolm Anderson <malcolm.b.anderson@gmail.com>
2023-09-16 23:38:40 +0000

    verbosity ver 4

--- args_01.py
+++ args_01.py
@@ -6,13 +6,21 @@ parser = argparse.ArgumentParser()
 # parser.add_argument("echo", help="echo the string you passed into the program")
 parser.add_argument(
     "square", type=int, help="display a square of a given number")
+# parser.add_argument(
+#     "-v", "--verbose", help="increase output verbosity", action="store_true")
 parser.add_argument(
-    "-v", "--verbose", help="increase output verbosity", action="store_true")
+    "-v", "--verbosity", type=int, help="increase output verbosity")
 
 args = parser.parse_args()
 # print(args.echo)
 answer = args.square**2
-if args.verbose:
+# if args.verbose:
+#     print(f"the square of {args.square} equals {answer}")
+# else :
+#     print(answer)
+if args.verbosity == 2:
     print(f"the square of {args.square} equals {answer}")
+elif args.verbosity == 1:
+    print(f"{args.square}^2 == {answer}")
 else:
     print(answer)
