faf183f48f659d0184388262c1b938a79509c5db
Malcolm Anderson <malcolm.b.anderson@gmail.com>
2023-09-16 23:59:38 +0000

    program is now base exponent

--- args_01.py
+++ args_01.py
@@ -4,8 +4,10 @@ import argparse
 parser = argparse.ArgumentParser()
 
 # parser.add_argument("echo", help="echo the string you passed into the program")
-parser.add_argument(
-    "square", type=int, help="display a square of a given number")
+# parser.add_argument(
+parser.add_argument("x", type=int, help="the base")
+parser.add_argument("y", type=int, help="the exponent")
+#     "square", type=int, help="display a square of a given number")
 # parser.add_argument(
 #     "-v", "--verbose", help="increase output verbosity", action="store_true")
 # parser.add_argument(
@@ -15,14 +17,20 @@ parser.add_argument(
 
 args = parser.parse_args()
 # print(args.echo)
-answer = args.square**2
+answer = args.x**args.y
 # if args.verbose:
 #     print(f"the square of {args.square} equals {answer}")
 # else :
 #     print(answer)
+# if args.verbosity >= 2:
+#     print(f"the square of {args.square} equals {answer}")
+# elif args.verbosity == 1:
+#     print(f"{args.square}^2 == {answer}")
+# else:
+#     print(answer)
 if args.verbosity >= 2:
-    print(f"the square of {args.square} equals {answer}")
+    print(f"{args.x} to the power {args.y} equals {answer}")
 elif args.verbosity == 1:
-    print(f"{args.square}^2 == {answer}")
+    print(f"{args.x}^{args.y} == {answer}")
 else:
     print(answer)
