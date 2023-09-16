c99ace715733f7d87e2ba08c6da94affdbb8c147
Malcolm Anderson <malcolm.b.anderson@gmail.com>
2023-09-16 23:22:07 +0000

    add type info

--- args_01.py
+++ args_01.py
@@ -3,7 +3,10 @@
 import argparse
 parser = argparse.ArgumentParser()
 
-parser.add_argument("echo", help="echo the string you passed into the program")
+# parser.add_argument("echo", help="echo the string you passed into the program")
+parser.add_argument(
+    "square", help="display a square of a given number", type=int)
 
 args = parser.parse_args()
-print(args.echo)
+# print(args.echo)
+print(args.square**2)
